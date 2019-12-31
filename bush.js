4367232d52ad16e2c70f5b02fbad91d93628a7d7
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2019-12-31 09:50:52 +0200

    Remove randomization

--- bush.js
+++ bush.js
@@ -1,8 +1,8 @@
 AFRAME.registerComponent('bush', {
   init() {
     const children = `
-      <a-entity gltf-model="#bush-model" scale="2 2 2" position="0 -1.2 0"></a-entity>
-      <a-cylinder radius="1" height="1.5" material="color: #888; opacity: 0.0; transparent: true;" position="0 -0.6 0" trigger-cylinder="selector: [frisbee]; payload: 0.9;"></a-cylinder>
+      <a-entity gltf-model="#bush-model" scale="2 2 2" position="0 -0.2 0"></a-entity>
+      <a-cylinder radius="1.1" height="1.5" material="color: #888; opacity: 0.0; transparent: true;" position="0 0.4 0" trigger-cylinder="selector: [frisbee]; payload: 0.9;"></a-cylinder>
     `;
 
     this.el.innerHTML = children;
---
+++ map-design-scripts.js
@@ -0,0 +1,7 @@
+[...Array(10)].map((_, i) => `<a-oak-tree id="oak-${i}" position="5 0 ${i}"></a-oak-tree>`).join('');
+
+[...Array(10)].map((_, i) => `<a-pine-tree id="pine-${i}" position="-5 0 ${i}"></a-pine-tree>`).join('');
+
+[...Array(10)].map((_, i) => `<a-bush id="bush-${i}" position="3 0 ${i}"></a-bush>`).join('');
+
+[...Array(10)].map((_, i) => `<a-rock id="rock-${i}" position="-3 0 ${i}"></a-rock>`).join('');
